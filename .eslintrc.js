f04138608df0453398bd60df23350986933b6042
Keshava Kotri <keshav143420@gmail.com>
2021-08-14 01:16:48 +0530

    add eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,15 @@
 module.exports = {
-  'env': {
-    'es2021': true,
-    'node': true,
+  env: {
+    es2021: true,
+    node: true,
   },
-  'extends': [
-    'google',
-  ],
-  'parser': '@typescript-eslint/parser',
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+  extends: ['google'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 12,
+    sourceType: 'module',
   },
-  'plugins': [
-    '@typescript-eslint',
-  ], 'ignorePatterns': ['/dist', '/node_modules', '/coverage'],
-  'rules': {
-  },
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['/dist', '/node_modules', '/coverage'],
+  rules: {},
 };
--- src/CalcClass.ts
+++ src/CalcClass.ts
@@ -1,7 +1,15 @@
- class Calculator {
-    static add(firstNumber: number, secondNumber: number): number {
-      return firstNumber + secondNumber;
-    }
+/**
+ * Calculator Class.
+ */
+class Calculator {
+  /**
+   * Adds two numbers together.
+   * @param {number} firstNumber The first number.
+   * @param {number} secondNumber The second number.
+   * @return {number} The sum of the two numbers.
+   */
+  static add(firstNumber: number, secondNumber: number): number {
+    return firstNumber + secondNumber;
   }
-  export default Calculator;
-  
+}
+export default Calculator;
